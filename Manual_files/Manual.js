6e5502bef9fe1eec0c2e485a5b3f1f813f39f203
Mario Schreiner <mario.schreiner.new@gmail.com>
2010-03-27 03:58:14 +0100

    Some modifications, added manual page

--- Manual_files/Manual.js
+++ Manual_files/Manual.js
@@ -1,9 +1,11 @@
-// Created by iWeb 2.0.4 local-build-20100326
+// Created by iWeb 2.0.4 local-build-20100327
 
-setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({reflection_3:new IWReflection({opacity:0.50,offset:1.00}),reflection_1:new IWReflection({opacity:0.50,offset:1.00}),reflection_5:new IWReflection({opacity:0.50,offset:1.00}),reflection_0:new IWReflection({opacity:0.50,offset:1.00}),reflection_4:new IWReflection({opacity:0.50,offset:1.00}),reflection_2:new IWReflection({opacity:0.50,offset:1.00}),shadow_0:new IWShadow({blurRadius:10,offset:new IWPoint(4.2426,4.2426),color:'#000000',opacity:0.750000})});registry.applyEffects();}
+function hostedOnDM()
 {return false;}
 function onPageLoad()
 {loadMozillaCSS('Manual_files/ManualMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');applyEffects()}
 function onPageUnload()
 {Widget.onunload();}
--- Screenshots_files/Screenshots.js
+++ Screenshots_files/Screenshots.js
@@ -1,15 +1,15 @@
-// Created by iWeb 2.0.4 local-build-20100326
+// Created by iWeb 2.0.4 local-build-20100327
 
 function createMediaStream_id3()
-{return IWCreatePhotocast("file://localhost/Users/BlackWolf/Sites/RAU/RAR-Archive_Utility/Screenshots_files/rss.xml",false,true);}
+{return IWCreatePhotocast("file://localhost/Users/BlackWolf/Sites/rau6/RAR-Archive_Utility/Screenshots_files/rss.xml",false,true);}
 function initializeMediaStream_id3()
-{createMediaStream_id3().load('file://localhost/Users/BlackWolf/Sites/RAU/RAR-Archive_Utility',function(imageStream)
+{createMediaStream_id3().load('file://localhost/Users/BlackWolf/Sites/rau6/RAR-Archive_Utility',function(imageStream)
 {var entryCount=imageStream.length;var headerView=widgets['widget1'];headerView.setPreferenceForKey(imageStream.length,'entryCount');NotificationCenter.postNotification(new IWNotification('SetPage','id3',{pageIndex:0}));});}
 function layoutMediaGrid_id3(range)
-{createMediaStream_id3().load('file://localhost/Users/BlackWolf/Sites/RAU/RAR-Archive_Utility',function(imageStream)
+{createMediaStream_id3().load('file://localhost/Users/BlackWolf/Sites/rau6/RAR-Archive_Utility',function(imageStream)
 {if(range==null)
 {range=new IWRange(0,imageStream.length);}
-IWLayoutPhotoGrid('id3',new IWPhotoGridLayout(3,new IWSize(158,158),new IWSize(158,35),new IWSize(196,208),27,27,0,new IWSize(2,2)),new IWEmptyStroke(),imageStream,range,null,null,1.000000,null,'Media/slideshow.html','widget1','widget2','widget3')});}
+IWLayoutPhotoGrid('id3',new IWPhotoGridLayout(3,new IWSize(158,158),new IWSize(158,37),new IWSize(196,210),27,27,0,new IWSize(2,2)),new IWEmptyStroke(),imageStream,range,null,null,1.000000,null,'Media/slideshow.html','widget1','widget2','widget3')});}
 function relayoutMediaGrid_id3(notification)
 {var userInfo=notification.userInfo();var range=userInfo['range'];layoutMediaGrid_id3(range);}
 function onStubPage()
